f0ff3596cf246a92baa7fcdce31d0296d08a94d7
Edward Kmett <ekmett@gmail.com>
2020-09-08 07:38:58 -0700

    sequenceUnliftIO

--- par/src/Par/Hole.hs
+++ par/src/Par/Hole.hs
@@ -1,17 +1,21 @@
-{-# Language MagicHash #-}
+{-# Language BangPatterns #-}
+{-# Language BlockArguments #-}
 {-# Language ForeignFunctionInterface #-}
+{-# Language GHCForeignImportPrim #-}
+{-# Language MagicHash #-}
 {-# Language UnboxedTuples #-}
 {-# Language UnliftedFFITypes #-}
-{-# Language BangPatterns #-}
-{-# Language GHCForeignImportPrim #-}
 
 -- | Based on <http://www.twanvl.nl/blog/haskell/unsafe-sequence>
 module Par.Hole 
   ( newHole
   , setHole
   , unsafeSetField
+  , sequenceIO
+  , sequenceUnliftIO
   ) where
 
+import Control.Monad.IO.Unlift
 import GHC.Types
 import GHC.Prim
 
@@ -36,3 +40,33 @@ unsafeSetField (I# i) !x y =
   case unsafeSetField# i (unsafeCoerce# x :: Any) (unsafeCoerce# y :: Any) of
     (##) -> pure ()
 {-# INLINEABLE unsafeSetField #-}
+
+sequenceUnliftIO :: MonadUnliftIO m => [m a] -> m [a]
+sequenceUnliftIO [] = pure []
+sequenceUnliftIO (mx0:xs0) = withRunInIO \run -> do
+    x0 <- run mx0
+    let front = x0:[]
+    front <$ go run front xs0
+  where
+    go run back (mx:xs) = do
+      x <- run mx
+      let back' = x:[]
+      unsafeSetField 1 back back'
+      go run back' xs
+    go _ _ [] = pure ()
+{-# INLINEABLE sequenceUnliftIO #-}
+
+sequenceIO :: [IO a] -> IO [a]
+sequenceIO [] = pure []
+sequenceIO (mx0:xs0) = do
+    x0 <- mx0
+    let front = x0:[]
+    front <$ go front xs0
+  where
+    go back (mx:xs) = do
+      x <- mx
+      let back' = x:[]
+      unsafeSetField 1 back back'
+      go back' xs
+    go _ [] = pure ()
+{-# INLINEABLE sequenceIO #-}
