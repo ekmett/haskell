ccc6008cdb585ac6a9aca0e273451daac6a5c155
Edward Kmett <ekmett@gmail.com>
2020-09-21 03:36:01 -0700

    describe the adjunction between Some and Const

--- types/src/Data/Type/Some.hs
+++ types/src/Data/Type/Some.hs
@@ -1,11 +1,21 @@
-{-# Language PolyKinds #-}
-{-# Language TypeApplications #-}
+{-# language PolyKinds #-}
+{-# language RankNTypes #-}
+{-# language TypeApplications #-}
+{-# language TypeOperators #-}
+
+-- parametric colimits
+
 module Data.Type.Some 
   ( Some(Some)
   , singSome
   , someSing
+  , fromSC
+  , toSC
+  , rightSC
+  , leftSC
   ) where
 
+import Control.Applicative
 import Data.Type.Internal
 import Data.Some
 
@@ -16,3 +26,18 @@ someSing (Some (Sing a)) = a
 singSome :: a -> Some (Sing @a)
 singSome a = Some (SING a)
 {-# inline singSome #-}
+
+type f ~> g = forall i. f i -> g i
+infixr 0 ~>
+
+fromSC :: Some (Const a) -> a
+fromSC (Some (Const a)) = a
+
+toSC :: a -> Some (Const a)
+toSC a = Some (Const a)
+
+rightSC :: (Some f -> x) -> f ~> Const x
+rightSC k = Const . k . Some
+
+leftSC :: (f ~> Const x) -> Some f -> x
+leftSC k (Some f) = getConst (k f)
